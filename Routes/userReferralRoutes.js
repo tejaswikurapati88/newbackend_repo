76acdf814b94689d451f6b5282ff4ce80954de1a
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-03-06 13:56:45 +0530

    add controller to get referals

--- Routes/userReferralRoutes.js
+++ Routes/userReferralRoutes.js
@@ -1,9 +1,14 @@
-const express=require("express");
-const router=express.Router();
-const {sendReferralEmail}=require("../Controllers/userReferralController");
+const express = require("express");
+const router = express.Router();
+const {
+  sendReferralEmail,
+  getAllReferrrals,
+} = require("../Controllers/userReferralController");
 
 //Route for sending referal link
 router.post("/send-referral-email", sendReferralEmail);
 
+//Route for getting referrals
+router.get("/get-referrals", getAllReferrrals);
 
-module.exports=router;
+module.exports = router;
